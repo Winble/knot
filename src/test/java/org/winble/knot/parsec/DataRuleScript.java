40ca9c622d3a4ea2806dcaa5158da2a64abbe20c
bowenzhang <bowenzhang@trip.com>
2022-12-29 22:05:01 +0800

    add DataRuleScript

--- src/test/java/org/winble/knot/parsec/DataRuleScript.java
+++ src/test/java/org/winble/knot/parsec/DataRuleScript.java
@@ -1,5 +1,10 @@
 package org.winble.knot.parsec;
 
+import org.winble.knot.parsec.type.Parser;
+
+import static org.winble.knot.parsec.Combinators.*;
+import static org.winble.knot.parsec.Parsers.*;
+
 /**
  * @author bowenzhang
  * Create on 2022/12/29
@@ -8,8 +13,10 @@ package org.winble.knot.parsec;
  * T ::= true || S | true
  * F ::= false && S | false
  * T ::= !F
+ *
+ * true && (null == _content_manage_user || ("none" == _content_manage_user || ["none"] == _content_manage_user)) && (null == _content_manage_platform || (_content_manage_platform instanceof List ? (!_content_manage_platform.isEmpty() && ["TDS","VBK","6381"].containsAll(_content_manage_platform)) : ["TDS","VBK","6381"].contains(_content_manage_platform)))
  */
 public class DataRuleScript {
-
     
+//    public final static Parser<Boolean> equalExpression = ;
 }
