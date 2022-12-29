c7d76d44998709cd2a9550aa021de99a998dfbd4
bowenzhang <bowenzhang@trip.com>
2022-12-29 21:57:32 +0800

    add DataRuleScript

---
+++ src/test/java/org/winble/knot/parsec/DataRuleScript.java
@@ -0,0 +1,15 @@
+package org.winble.knot.parsec;
+
+/**
+ * @author bowenzhang
+ * Create on 2022/12/29
+ * {true | false}
+ * S ::= T && S | F || S | T | F | !S | (S)
+ * T ::= true || S | true
+ * F ::= false && S | false
+ * T ::= !F
+ */
+public class DataRuleScript {
+
+    
+}
